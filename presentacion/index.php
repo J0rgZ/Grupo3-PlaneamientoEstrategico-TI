395037a2194f4e62d136feab63f8c633200a933c
CMirianG <mc2021071083@virtual.upt.pe>
2024-10-27 17:16:32 -0500

    Update index.php

--- presentacion/index.php
+++ presentacion/index.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start(); // Iniciar la sesión
-require '../presentacion/estilos1.css';
 
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
+    require '../datos/conexion.php';
     exit();
 }
 
