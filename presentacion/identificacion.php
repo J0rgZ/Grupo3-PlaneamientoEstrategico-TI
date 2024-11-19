66563271b35b5d8729fb2d756503fc7ac6833601
GabrielFMH <84587604+GabrielFMH@users.noreply.github.com>
2024-11-19 10:56:15 -0500

    indentificacionv3

--- presentacion/identificacion.php
+++ presentacion/identificacion.php
@@ -16,45 +16,260 @@
             <p>Tras el análisis realizado habiéndose identificado las oportunidades, amenazas, fortalezas y debilidades, es momento de identificar la estrategia que debe seguir su empresa para el logro de sus objetivos empresariales.</p>
             <p>Se trata de realizar una Matriz Cruzada tal y como se refleja en el siguiente diagrama para identificar la estrategia más conveniente a llevar a cabo.</p>
         </div>
-
+        <img src="../img/matrizdafo.png" alt="matriz_dafo" width="1000" height="auto">
         <div class="valores-examples">
-            <h2>Matriz DOFA</h2>
-            <div class="column">
-                <h3>Fortalezas</h3>
-                <ul>
-                    <li>Fortaleza 1</li>
-                    <li>Fortaleza 2</li>
-                    <li>Fortaleza 3</li>
-                    <li>Fortaleza 4</li>
-                </ul>
-            </div>
-            <div class="column">
-                <h3>Debilidades</h3>
-                <ul>
-                    <li>Debilidad 1</li>
-                    <li>Debilidad 2</li>
-                    <li>Debilidad 3</li>
-                    <li>Debilidad 4</li>
-                </ul>
-            </div>
-            <div class="column">
-                <h3>Oportunidades</h3>
-                <ul>
-                    <li>Oportunidad 1</li>
-                    <li>Oportunidad 2</li>
-                    <li>Oportunidad 3</li>
-                    <li>Oportunidad 4</li>
-                </ul>
-            </div>
-            <div class="column">
-                <h3>Amenazas</h3>
-                <ul>
-                    <li>Amenaza 1</li>
-                    <li>Amenaza 2</li>
-                    <li>Amenaza 3</li>
-                    <li>Amenaza 4</li>
-                </ul>
-            </div>
+            <div class="matriz-general">
+            <p>Según ha ido cumplimentando en las fases anteriores, los factores internos y externos  de su empresa son los siguientes: </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Debilidades</th>
+                        <th>Amenazas</th>
+                        <th>Fortalezas</th>
+                        <th>Oportunidades</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                        <td><input type="text"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+            <div class="matriz-fortalezas-oportunidades">
+            <h2>Matriz Fortalezas vs Oportunidades</h2>
+            <p>Las fortalezas se usan para tomar ventaja en cada una las oportunidades.<br>0=En total desacuerdo, 1= No está de acuerdo, 2= Está de acuerdo, 3= Bastante de acuerdo y 4=En total acuerdo</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fortalezas</th>
+                        <th>O1</th>
+                        <th>O2</th>
+                        <th>O3</th>
+                        <th>O4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>F1</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F2</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F3</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F4</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="matriz-fortalezas-amenazas">
+            <h2>Matriz Fortalezas vs Amenazas</h2>
+            <p>Las fortalezas evaden el efecto negativo de las amenazas.<br>0=En total desacuerdo, 1= No está de acuerdo, 2= Está de acuerdo, 3= Bastante de acuerdo y 4=En total acuerdo</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fortalezas</th>
+                        <th>A1</th>
+                        <th>A2</th>
+                        <th>A3</th>
+                        <th>A4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>F1</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F2</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F3</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>F4</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="matriz-debilidades-oportunidades">
+            <h2>Matriz Debilidades vs Oportunidades</h2>
+            <p>Superamos las debilidades tomando ventaja de las oportunidades.<br>0=En total desacuerdo, 1= No está de acuerdo, 2= Está de acuerdo, 3= Bastante de acuerdo y 4=En total acuerdo</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Debilidades</th>
+                        <th>O1</th>
+                        <th>O2</th>
+                        <th>O3</th>
+                        <th>O4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>D1</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D2</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D3</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D4</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="matriz-debilidades-amenazas">
+            <h2>Matriz Debilidades vs Amenazas</h2>
+            <p>Las debilidades intensifican notablemente el efecto negativo de las amenazas.<br>0=En total desacuerdo, 1= No está de acuerdo, 2= Está de acuerdo, 3= Bastante de acuerdo y 4=En total acuerdo</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Debilidades</th>
+                        <th>A1</th>
+                        <th>A2</th>
+                        <th>A3</th>
+                        <th>A4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>D1</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D2</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D3</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>D4</td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                        <td><input type="number" min="0" max="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
             <div class="sintesis-resultados">
             <h2>Síntesis de Resultados</h2>
             <table>
@@ -70,25 +285,25 @@
                     <tr>
                         <td>FO</td>
                         <td>Estrategia Ofensiva</td>
-                        <td>0</td>
+                        <td><input type="number" min="0" max="4"></td>
                         <td>Deberá adoptar estrategias de crecimiento</td>
                     </tr>
                     <tr>
                         <td>AF</td>
                         <td>Estrategia Defensiva</td>
-                        <td>0</td>
+                        <td><input type="number" min="0" max="4"></td>
                         <td>La empresa está preparada para enfrentarse a las amenazas</td>
                     </tr>
                     <tr>
                         <td>AD</td>
                         <td>Estrategia de Supervivencia</td>
-                        <td>0</td>
+                        <td><input type="number" min="0" max="4"></td>
                         <td>Se enfrenta a amenazas externas sin las fortalezas necesarias para luchar con la competencia</td>
                     </tr>
                     <tr>
                         <td>OD</td>
                         <td>Estrategia de Reorientación</td>
-                        <td>0</td>
+                        <td><input type="number" min="0" max="4"></td>
                         <td>La empresa no puede aprovechar las oportunidades porque carece de preparación adecuada</td>
                     </tr>
                 </tbody>
@@ -105,4 +320,3 @@
     </div>
 </body>
 </html>
-
